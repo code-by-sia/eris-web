43e77a6d10a430872a8fade3866802a89f6023ff
Siamand <me@siamand.cc>
2023-02-20 20:42:23 +0100

    feat: fix build

--- src/store/books-api.js
+++ src/store/books-api.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
 export const booksApi = createApi({
     reducerPath: 'booksApi',
